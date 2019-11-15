421e1da4b78760246bf996abc9d84a16bcfb0608
frikky <fredrik_9490@hotmail.com>
2019-11-14 20:48:18 -0500

    Fixed with internal variables

--- fsf.go
+++ fsf.go
@@ -1,11 +1,7 @@
 package fsf
 
 import (
-	//"cloud.google.com/go/firestore"
 	"fmt"
-	"os"
-	//"path/filepath"
-	//"golang.org/x/tools/go/packages"
 	"log"
 	"reflect"
 	"strconv"
@@ -69,89 +65,34 @@ func iterate(subValue interface{}) interface{} {
 		//	return values[0]
 		//}
 
-		log.Printf("TYPE: %s", curType)
+		//log.Printf("TYPE: %s", curType)
 		if curType == "fsf.ArrayValue" || strings.Contains(curType, "ArrayValue") {
 			values[i] = iterate(values[i])
 		} else if curType == "fsf.MapValue" || strings.Contains(curType, "MapValue") {
 			values[i] = iterate(values[i])
-		} else if curType == "[]fsf.Value" || strings.Contains(curType, "[]main.Value") {
-			//log.Printf("GLOB: %#v",)
-			//test := castType.Type()
-			//log.Println(test)
-			//log.Println(test.Elem())
-			//test2 := test.Elem()
-			//_ = test2
-
-			//tmp := reflect.ValueOf(values[i])
-			//switch tmp.Kind() {
-			//case reflect.Slice, reflect.Array:
-			//	log.Println(tmp.Kind())
-			//	result := make([]interface{}, tmp.Len())
-			//	for i := 0; i < tmp.Len(); i++ {
-			//		result[i] = tmp.Index(i).Interface()
-			//	}
-
-			//	log.Println(result)
-			//	log.Println(tmp.Len())
-			//default:
-			//	log.Println("NOT IN HERE")
-			//}
-
-			//tmpvalue := reflect.MakeSlice(reflect.ValueOf(values[i]), 1, 1)
-			//log.Println(len(castType))
-			//log.Printf("TYPE: %#v", castType)
-			//test := castType
-			//log.Printf("%#v", test)
-
-			//vp := reflect.New(reflect.TypeOf(values[i]))
-			//vp.Elem().Set(reflect.ValueOf(values[i]))
-
-			//tmpvalues := make([]interface{}, tmp.Len())
-			//log.Println(tmp.Interface().([]Value))
-			//log.Printf("%#v", values[i].([]Value))
-			//for iter, sub := range values[i].([]Value) {
-
-			//log.Printf("%#v", vp)
-			//for iter, sub := range tmp.Interface().([]Value) {
-			//	log.Printf("%#v", sub)
-			//	tmpvalues[iter] = iterate(sub, main)
-
-			//	// FIXME - remove this
-			//	break
-			//	log.Printf("IN ITER: %#v", tmpvalues[iter])
-			//}
-
-			//values[i] = tmpvalues
-
+		} else if curType == "[]fsf.Value" {
 			tmpvalues := make([]interface{}, len(values[i].([]Value)))
 			//log.Printf("%#v", values[i].([]Value))
 			for iter, sub := range values[i].([]Value) {
-				log.Printf("%#v", sub)
 				tmpvalues[iter] = iterate(sub)
 
 				// FIXME - remove this
-				break
-				log.Printf("IN ITER: %#v", tmpvalues[iter])
 			}
 
-			values[i] = tmpvalues
+			values[0] = tmpvalues
+			//return tmpvalues
 		} else if curType == "fsf.Fields" || strings.Contains(curType, "Fields") {
 			values[i] = iterate(values[i])
 		} else if curType == "fsf.IntegerValue" || strings.Contains(curType, "IntegerValue") {
-			normalSet = true
-
-			if len(values[i].(IntegerValue).IntegerValue) == 0 {
-				toBeRemoved = append(toBeRemoved, i)
-				continue
-			}
 
 			tmp, err := strconv.Atoi(values[i].(IntegerValue).IntegerValue)
 			if err != nil {
 				toBeRemoved = append(toBeRemoved, i)
-				log.Printf("Error handling integervalue for field %s. FieldValue: %s", fieldName, values[i].(IntegerValue).IntegerValue)
+				//log.Printf("Error handling integervalue for field %s. FieldValue: %s", fieldName, values[i].(IntegerValue).IntegerValue)
 				continue
 			}
 
+			normalSet = true
 			normalValues[fieldName] = tmp
 		} else if curType == "fsf.StringValue" || strings.Contains(curType, "StringValue") {
 			if len(values[i].(StringValue).StringValue) > 0 {
@@ -165,18 +106,17 @@ func iterate(subValue interface{}) interface{} {
 		}
 	}
 
-	//log.Println(toBeRemoved)
-	//if len(toBeRemoved) > 0 {
-	//	log.Printf("%#v", values[toBeRemoved[0]])
-	//}
-
 	if normalSet {
-		values[0] = normalValues
+		//log.Printf("PRE: %#v", values)
+		// FIXME - this might be wrong
+		log.Printf("NORMAL %#v", normalValues)
+		return normalValues
 	}
 
 	//log.Printf("Len: %d", len(values))
 	if len(values) > 1 {
-		log.Printf("Might be an error if I hit this one. Returning first value.\nItem: %#v", values)
+		//log.Printf("Might be an error if I hit this one. Returning first value.\nItem: %#v", values)
+		//log.Printf("\n%#v", values)
 	}
 
 	return values[0]
@@ -184,16 +124,7 @@ func iterate(subValue interface{}) interface{} {
 
 // passedField takes arrayValue
 func GetInterface(subValue interface{}) map[string]interface{} {
-	//log.Println(packages.Load(main, "bytes", "unicode"))
 	var err error
-	pwd, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	fmt.Println(pwd)
-
 	v := reflect.ValueOf(subValue)
 
 	newValues := make(map[string]interface{})
@@ -210,7 +141,7 @@ func GetInterface(subValue interface{}) map[string]interface{} {
 		if curType == "fsf.IntegerValue" {
 			newValues[fieldName], err = strconv.Atoi(values[i].(IntegerValue).IntegerValue)
 			if err != nil {
-				log.Printf("Error handling integervalue for field %s", fieldName)
+				//log.Printf("Error handling integervalue for field %s", fieldName)
 				continue
 			}
 		} else if curType == "fsf.StringValue" {
