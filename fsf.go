87aba26ab5db9d53b707857da7f94065376ecfb8
frikky <fredrik_9490@hotmail.com>
2019-11-14 21:28:35 -0500

    Added more types

--- fsf.go
+++ fsf.go
@@ -11,9 +11,26 @@ import (
 type IntegerValue struct {
 	IntegerValue string `json:"integerValue"`
 }
-
 type StringValue struct {
 	StringValue string `json:"stringValue"`
+}
+type NullValue struct {
+	NulLValue string `json:"nullValue"`
+}
+type BooleanValue struct {
+	BooleanValue string `json:"booleanValue"`
+}
+type DoubleValue struct {
+	DoubleValue string `json:"doubleValue"`
+}
+type TimestampValue struct {
+	TimestampValue string `json:"timestampValue"`
+}
+type BytesValue struct {
+	BytesValue string `json:"bytesValue"`
+}
+type ReferenceValue struct {
+	ReferenceValue string `json:"referenceValue"`
 }
 
 type ArrayValue struct {
@@ -22,10 +39,15 @@ type ArrayValue struct {
 
 // This is inside an array again. Always confuse.
 type Value struct {
-	MapValue     MapValue     `json:"mapValue,omitempty"`
-	StringValue  StringValue  `json:"stringValue,omitempty"`
-	IntegerValue IntegerValue `json:"integerValue,omitempty"`
-	ArrayValue   ArrayValue   `json:"arrayValue,omitempty"`
+	MapValue       MapValue       `json:"mapValue,omitempty"`
+	StringValue    StringValue    `json:"stringValue,omitempty"`
+	IntegerValue   IntegerValue   `json:"integerValue,omitempty"`
+	ArrayValue     ArrayValue     `json:"arrayValue,omitempty"`
+	BooleanValue   BooleanValue   `json:"booleanValue,omitempty"`
+	DoubleValue    DoubleValue    `json:"doubleValue,omitempty"`
+	TimestampValue TimestampValue `json:"timestampValue,omitempty"`
+	BytesValue     BytesValue     `json:"bytesValue,omitempty"`
+	ReferenceValue ReferenceValue `json:"referenceValue,omitempty"`
 }
 
 type MapValue struct {
@@ -55,40 +77,36 @@ func iterate(subValue interface{}) interface{} {
 	normalValues := make(map[string]interface{})
 	normalSet := false
 
-	toBeRemoved := []int{}
 	for i := 0; i < v.NumField(); i++ {
 		values[i] = v.Field(i).Interface()
 		fieldName := typeOfS.Field(i).Name
 
 		curType := fmt.Sprintf("%s", reflect.TypeOf(values[i]))
-		//if curType == "[]interface {}" {
-		//	return values[0]
-		//}
-
-		//log.Printf("TYPE: %s", curType)
 		if curType == "fsf.ArrayValue" || strings.Contains(curType, "ArrayValue") {
 			values[i] = iterate(values[i])
 		} else if curType == "fsf.MapValue" || strings.Contains(curType, "MapValue") {
 			values[i] = iterate(values[i])
 		} else if curType == "[]fsf.Value" {
 			tmpvalues := make([]interface{}, len(values[i].([]Value)))
-			//log.Printf("%#v", values[i].([]Value))
 			for iter, sub := range values[i].([]Value) {
 				tmpvalues[iter] = iterate(sub)
-
-				// FIXME - remove this
 			}
 
 			values[0] = tmpvalues
-			//return tmpvalues
 		} else if curType == "fsf.Fields" || strings.Contains(curType, "Fields") {
 			values[i] = iterate(values[i])
 		} else if curType == "fsf.IntegerValue" || strings.Contains(curType, "IntegerValue") {
 
 			tmp, err := strconv.Atoi(values[i].(IntegerValue).IntegerValue)
 			if err != nil {
-				toBeRemoved = append(toBeRemoved, i)
-				//log.Printf("Error handling integervalue for field %s. FieldValue: %s", fieldName, values[i].(IntegerValue).IntegerValue)
+				continue
+			}
+
+			normalSet = true
+			normalValues[fieldName] = tmp
+		} else if curType == "fsf.DoubleValue" || strings.Contains(curType, "DoubleValue") {
+			tmp, err := strconv.ParseFloat(values[i].(DoubleValue).DoubleValue, 64)
+			if err != nil {
 				continue
 			}
 
@@ -96,27 +114,39 @@ func iterate(subValue interface{}) interface{} {
 			normalValues[fieldName] = tmp
 		} else if curType == "fsf.StringValue" || strings.Contains(curType, "StringValue") {
 			if len(values[i].(StringValue).StringValue) > 0 {
-				toBeRemoved = append(toBeRemoved, i)
 				normalSet = true
 				normalValues[fieldName] = values[i].(StringValue).StringValue
 			}
+		} else if curType == "fsf.NullValue" || strings.Contains(curType, "NullValue") {
+			normalValues[fieldName] = nil
+		} else if curType == "fsf.BooleanValue" || strings.Contains(curType, "BooleanValue") {
+			value := values[i].(BooleanValue).BooleanValue
+			if len(value) > 0 {
+				if value == "true" {
+					normalValues[fieldName] = true
+				} else if value == "false" {
+					normalValues[fieldName] = true
+				} else {
+					continue
+				}
+
+				normalSet = true
+			}
 		} else {
-			log.Printf("UNHANDLED TYPE: %s\nValue: %s, Fieldname: %s", curType, values[i], fieldName)
-			normalValues[fieldName] = values[i]
+			log.Printf("UNHANDLED TYPE: %s. Value: %s, Fieldname: %s", curType, values[i], fieldName)
+			values[i] = iterate(values[i])
+
+			// THis is just as a test. Idk if it will work or not for weird values lol
+			//normalSet = true
+			//normalValues[fieldName] = values[i]
 		}
 	}
 
 	if normalSet {
 		//log.Printf("PRE: %#v", values)
 		// FIXME - this might be wrong
-		log.Printf("NORMAL %#v", normalValues)
+		//log.Printf("NORMAL %#v", normalValues)
 		return normalValues
-	}
-
-	//log.Printf("Len: %d", len(values))
-	if len(values) > 1 {
-		//log.Printf("Might be an error if I hit this one. Returning first value.\nItem: %#v", values)
-		//log.Printf("\n%#v", values)
 	}
 
 	return values[0]
@@ -137,7 +167,7 @@ func GetInterface(subValue interface{}) map[string]interface{} {
 		fieldName := typeOfS.Field(i).Name
 
 		curType := fmt.Sprintf("%s", reflect.TypeOf(values[i]))
-		log.Printf("TYPE: %s", curType)
+		//log.Printf("TYPE: %s", curType)
 		if curType == "fsf.IntegerValue" {
 			newValues[fieldName], err = strconv.Atoi(values[i].(IntegerValue).IntegerValue)
 			if err != nil {
